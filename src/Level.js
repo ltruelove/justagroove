5f0109b9fe9f76e1c759cca153e1be96e4d08c46
Levi Truelove <levi@levitruelove.com>
2014-03-06 22:24:07 -0500

    Playing around with adding more balls to the screen.

--- src/Level.js
+++ src/Level.js
@@ -2,6 +2,10 @@ MainGame.Level = function(game) {
     this.game = game;
     this.arrowSprite = null;
     this.ballSprite = null;
+    this.currentBall = null;
+    this.ballGroup = null;
+    this.isQueued = false;
+    
     this.map = null;
     this.tileset = null;
     this.layer = null;
@@ -27,32 +31,36 @@ MainGame.Level.prototype = {
         this.game.stage.scale.maxWidth = 600;
         this.game.stage.scale.maxHeight = 720;
         this.game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
-        //this.game.load.tilemap("platforms", "/assets/tilemaps/level1.json", null, Phaser.Tilemap.TILED_JSON);
-        //this.game.load.image("land", "/assets/tilemaps/tiles_spritesheet.png");
-        //, this.tileWidth, this.tileHeight,144,0,1
-        //this.game.load.image('coin', '/assets/sprites/coinGold.png');
-        //game.load.audio('music', ['/resources/L1Audio.mp3']);
-        //this.game.load.image('L1BG', '/assets/img/level1bg.png');
-        //this.game.load.image('speech', '/assets/sprites/speech_bubble.png');
-        //this.game.load.atlas("enemies", "/resources/enemies.png",
-        //"/resources/enemies.json", null, Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
     },
     
     create: function() {
+        this.ballGroup = this.game.add.group();
         this.arrowSprite = this.game.add.sprite(this.game.world.centerX,585,'sprites','arrow');
-        this.ballSprite = this.game.add.sprite(this.game.world.centerX,585,'sprites','red_ball');
-
-        this.ballSprite.anchor.setTo(0.5,0.5);
-        this.ballSprite.body.collideWorldBounds = true;
-        this.ballSprite.body.bounce.setTo(1,1);
+        this.addBall();
 
         this.arrowSprite.pivot.x = 135;
         this.arrowSprite.pivot.y = 15;
         this.arrowSprite.rotation = 1.57;
     },
+
+    addBall: function(){
+        if(!this.isQueued){
+            this.isQueued = true;
+            var ballSprite = this.ballGroup.create(this.game.world.centerX,585,'sprites','red_ball');
+            ballSprite.hanging = false;
+            console.log(ballSprite.y);
+
+            ballSprite.anchor.setTo(0.5,0.5);
+            ballSprite.body.collideWorldBounds = true;
+            ballSprite.body.bounce.setTo(1,1);
+            this.currentBall = ballSprite;
+            console.log(this.currentBall.y);
+        }
+    },
     
     update: function(){
         this.arrowSprite.body.angularVelocity = 0;
+        this.game.physics.collide(this.ballGroup, this.ballGroup, this.ballCollide, null, this);
 
         if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
                 this.arrowSprite.body.angularVelocity = -150;
@@ -63,7 +71,17 @@ MainGame.Level.prototype = {
                 this.xVel += 10;
             }
         }else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            this.game.physics.velocityFromAngle(-180 + this.arrowSprite.angle, 500, this.ballSprite.body.velocity);
+            this.isQueued = false;
+            this.game.physics.velocityFromAngle(-180 + this.arrowSprite.angle, 500, this.currentBall.body.velocity);
+        }
+
+        if(this.currentBall.y <= 16){
+            this.currentBall.body.velocity = 0;
+            this.currentBall.hanging = true;
+
+            if(!this.isQueued){
+                this.addBall();
+            }
         }
     },
 
@@ -73,7 +91,20 @@ MainGame.Level.prototype = {
         this.game.debug.renderSpriteInfo(this.ballSprite, 32, 32);
         this.game.debug.renderText('rotation: ' + this.arrowSprite.rotation, 32, 200);
 
-    }  
+    },
+
+    ballCollide: function(ball1, ball2){
+        if(ball1.hanging || ball2.hanging){
+            ball1.body.velocity = 0;
+            ball2.body.velocity = 0;
+            ball1.hanging = true;
+            ball2.hanging = true;
+        }
+
+        if(!this.isQueued){
+            this.addBall();
+        }
+    }
 
 }
 
